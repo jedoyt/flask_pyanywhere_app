f8b96f4a90fca59fb8c81d69e5c323ff08fd53c3
unalivij <jed.unalivia@lexisnexisrisk.com>
2024-07-23 19:48:39 +0800

    Added about page with links to my social media. Added link to my OCR app hosted on Streamlit. Updated .gitignore

--- app/core.py
+++ app/core.py
@@ -5,3 +5,7 @@ bp = Blueprint('core', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/about')
+def about():
+    return render_template('about.html')
